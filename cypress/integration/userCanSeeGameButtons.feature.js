2cc2feca2df21a7579ae56f435cc77907e34706e
Jaime <jaime.cruz.hostalot@gmail.com>
2020-02-28 10:30:19 +0100

    added and refactored test

---
+++ cypress/integration/userCanSeeGameButtons.feature.js
@@ -0,0 +1,33 @@
+describe('User can see game interface', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000');
+    })
+    
+    it('Displays RPS game title', () => {
+        cy.contains('RPS Game')
+    })
+
+    It('Can see all 3 buttons', () => {
+        cy.contains('button#Paper');
+        cy.contains('button#Rock');
+        cy.contains('button#Scissors');
+    })
+
+    describe('User can win/draw/lose', () => {
+
+        it('Can choose the Rock button and win', () => {
+            cy.get('button#Rock').click();
+            cy.get('p#win-message').should('contain', 'You won! against Scissors')
+        })
+
+        it('Can choose the Scissors button and draw', () => {
+            cy.get('button#Scissors').click();
+            cy.get('p#win-message').should('contain', 'You draw! against Scissors')
+        })
+
+        it('Can choose the Paper button and lost', () => {
+            cy.get('button#Paper').click();
+            cy.get('p#win-message').should('contain', 'You lost! against Scissors')
+        })
+    })
+})
