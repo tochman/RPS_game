dfafe0a255657fa8103b08631e78021216fb01ed
Jaime <jaime.cruz.hostalot@gmail.com>
2020-03-28 14:12:51 +0100

    added sentences

--- src/player.jsx
+++ src/player.jsx
@@ -6,21 +6,22 @@ import Chance from 'chance';
 
 
 const playerTwoShape = () => {
-    let chance = new Chance()
-    let pickYourStudent = chance.pickone(["karro", "kayla", 
-    "emma", "jaime", "blake", "robin", "carlos", "Mr.lau", "philip", 
-    "janko", "pierre", "daniel", "hunter", "anish", "johan"])
-    return  pickYourStudent
-  }
-
-const pickYourSentence = () => {
-    let chance = new Chance()
-    let pickYourSentence = chance.pickone([
-    "Who's Your Daddy?", "Best demo ever, RIGHT!?", "Who's the man...I'm the man!",
-    "Get Thomas excited!", "Adi what the fuck do I need to do now?", "hold your horses cowboy!",
-    "I'm going to stop you right there!","Yippee-ki-yay motherfucker!"
-    ])   
-    return pickYourSentence
+  let chance = new Chance()
+  let pickYourStudent = chance.pickone(["Karro", "Kayla",
+    "Emma", "Jaime", "Blake", "Robin", "Carlos", "Mr.lau", "Philip",
+    "Janko", "Pierre", "Daniel", "Hunter", "Anish", "Johan"])
+  return pickYourStudent
 }
 
-export {pickYourSentence, playerTwoShape};
+const pickYourSentence = () => {
+  let chance = new Chance()
+  let pickYourSentence = chance.pickone([
+    "Who's Your Daddy?", "Best demo ever, RIGHT!?", "Who's the man...I'm the man!",
+    "Get Thomas excited!", "Adi what the fuck do I need to do now?", "hold your horses cowboy!",
+    "I'm going to stop you right there!", "Yippee-ki-yay motherfucker!", "That's the way the cookie crumbles",
+    "To be honest..Uhm", "Right...? right.. right right!", "BOOM!", "Isn't this amazing..? That's fucking amazing",
+    "Uhm.. Don't jump all at once", "Good stuff.. GOOD STUFF!"])
+  return pickYourSentence
+}
+
+export { pickYourSentence, playerTwoShape };
